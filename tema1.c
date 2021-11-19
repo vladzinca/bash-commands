f7e93927b7646504766eee1087278fa4b9c83a16
vladzinca <vlad_@VladsROG.localdomain>
2021-11-20 00:53:41 +0200

    v1.1 Optimized and commented main function

--- tema1.c
+++ tema1.c
@@ -137,6 +137,7 @@ void rm(Dir *parent, char *name)
 	if (parent->head_children_files == NULL)
 	{
 		printf("Could not find the file\n");
+		free(name);
 		return;
 	}
 	int flag = 1;
@@ -149,6 +150,7 @@ void rm(Dir *parent, char *name)
 			parent->head_children_files = NULL;
 			free(aux->name);
 			free(aux);
+			free(name);
 			return;
 		}
 		else
@@ -157,6 +159,7 @@ void rm(Dir *parent, char *name)
 			parent->head_children_files = parent->head_children_files->next;
 			free(aux->name);
 			free(aux);
+			free(name);
 			return;
 		}
 	}
@@ -164,6 +167,7 @@ void rm(Dir *parent, char *name)
 	if (aux->next == NULL)
 	{
 		printf("Could not find the file\n");
+		free(name);
 		return;
 	}
 	else
@@ -177,6 +181,7 @@ void rm(Dir *parent, char *name)
 				aux->next = aux->next->next;
 				free(aux2->name);
 				free(aux2);
+				free(name);
 				return;
 			}
 			aux = aux->next;
@@ -188,12 +193,14 @@ void rm(Dir *parent, char *name)
 			aux->next = NULL;
 			free(aux2->name);
 			free(aux2);
+			free(name);
 			return;
 		}
 	}
 	if (flag == 1)
 	{
 		printf("Could not find the file\n");
+		free(name);
 		return;
 	}
 }
@@ -203,6 +210,7 @@ void rmdir(Dir *parent, char *name)
 	if (parent->head_children_dirs == NULL)
 	{
 		printf("Could not find the dir\n");
+		free(name);
 		return;
 	}
 	int flag = 1;
@@ -239,6 +247,7 @@ void rmdir(Dir *parent, char *name)
 			parent->head_children_dirs = NULL;
 			free(aux->name);
 			free(aux);
+			free(name);
 			return;
 		}
 		else
@@ -272,6 +281,7 @@ void rmdir(Dir *parent, char *name)
 			parent->head_children_dirs = parent->head_children_dirs->next;
 			free(aux->name);
 			free(aux);
+			free(name);
 			return;
 		}
 	}
@@ -279,6 +289,7 @@ void rmdir(Dir *parent, char *name)
 	if (aux->next == NULL)
 	{
 		printf("Could not find the dir\n");
+		free(name);
 		return;
 	}
 	else
@@ -292,6 +303,7 @@ void rmdir(Dir *parent, char *name)
 				aux->next = aux->next->next;
 				free(aux2->name);
 				free(aux2);
+				free(name);
 				return;
 			}
 			aux = aux->next;
@@ -303,12 +315,14 @@ void rmdir(Dir *parent, char *name)
 			aux->next = NULL;
 			free(aux2->name);
 			free(aux2);
+			free(name);
 			return;
 		}
 	}
 	if (flag == 1)
 	{
 		printf("Could not find the dir\n");
+		free(name);
 		return;
 	}
 }
@@ -320,12 +334,17 @@ void cd(Dir **target, char *name)
 		if ((*target)->parent != NULL)
 			(*target) = (*target)->parent;
 		else
+		{
+			free(name);
 			return;
+		}
+		free(name);
 		return;
 	}
 	if ((*target)->head_children_dirs == NULL)
 	{
 		printf("No directories found!\n");
+		free(name);
 		return;
 	}
 	int flag = 1;
@@ -333,12 +352,14 @@ void cd(Dir **target, char *name)
 	{
 		flag = 0;
 		(*target) = (*target)->head_children_dirs;
+		free(name);
 		return;
 	}
 	Dir *aux = (*target)->head_children_dirs;
 	if (aux->next == NULL)
 	{
 		printf("No directories found!\n");
+		free(name);
 		return;
 	}
 	else
@@ -349,6 +370,7 @@ void cd(Dir **target, char *name)
 			{
 				flag = 0;
 				(*target) = aux;
+				free(name);
 				return;
 			}
 			aux = aux->next;
@@ -357,12 +379,14 @@ void cd(Dir **target, char *name)
 		{
 			flag = 0;
 			(*target) = aux;
+			free(name);
 			return;
 		}
 	}
 	if (flag == 1)
 	{
 		printf("No directories found!\n");
+		free(name);
 		return;
 	}
 }
@@ -399,34 +423,6 @@ char *pwd(Dir *target)
 	}
 	free(sirulPwd);
 	return auxPwd;
-	// char* aux;
-	// if (target->parent == NULL)
-	// {
-	// 	aux = malloc(6*sizeof(char));
-	// 	strcpy(aux, "/home");
-	// 	return aux;
-	// }
-	// else
-	// {
-	// 	if (aux == NULL)
-	// 	{
-	// 		aux = malloc(1+sizeof(aux)+strlen(target->name));
-	// 	}
-	// 	else
-	// 	// char * aux;
-	// 	// char * altaux = malloc(1000*sizeof(char));
-	// 	// strcpy(altaux, pwd(target->parent));
-	// 	// char * aux = malloc((10+strlen(target->name))*sizeof(char));
-	// 		aux = realloc(aux, 1000+strlen(target->name));
-	// 	// char *aux = malloc((2 + strlen(pwd(target->parent)) + strlen(target->name)) * sizeof(char));
-	// 	strcpy(aux, pwd(target->parent));
-	// 	// char *aux2;
-	// 	// strcat(strcat(aux, "/"), target->name);
-	// 	// strcpy(aux2, pwd(target->parent));
-
-	// 	// free(aux);
-	// 	return strcat(strcat(aux, "/"), target->name);
-	// }
 }
 
 void stop(Dir *target)
@@ -517,6 +513,7 @@ void mv(Dir *parent, char *oldname, char *newname)
 	{
 		/* daca si dir si file sunt goale */
 		printf("File/Director not found\n");
+		free(oldname);
 		free(newname);
 		return;
 	}
@@ -573,6 +570,7 @@ void mv(Dir *parent, char *oldname, char *newname)
 			{
 				rmdir(parent, oldname);
 				mkdir(parent, newname);
+				free(oldname);
 				return;
 			}
 			else
@@ -581,6 +579,7 @@ void mv(Dir *parent, char *oldname, char *newname)
 				if (fileeplin == 0)
 				{
 					printf("File/Director already exists\n");
+					free(oldname);
 					free(newname);
 					return;
 				}
@@ -592,6 +591,7 @@ void mv(Dir *parent, char *oldname, char *newname)
 			if (fileeplin == 0)
 			{
 				printf("File/Director not found\n");
+				free(oldname);
 				free(newname);
 				return;
 			}
@@ -650,12 +650,14 @@ void mv(Dir *parent, char *oldname, char *newname)
 			{
 				rm(parent, oldname);
 				touch(parent, newname);
+				free(oldname);
 				return;
 			}
 			else
 			{
 				/* daca newname exista deja in file/dir, iar lista de directoare s-a verificat deja */
 				printf("File/Director already exists\n");
+				free(oldname);
 				free(newname);
 				return;
 			}
@@ -664,143 +666,122 @@ void mv(Dir *parent, char *oldname, char *newname)
 		{
 			/* daca nu exista oldname in file */
 			printf("File/Director not found\n");
+			free(oldname);
 			free(newname);
 			return;
 		}
 	}
 }
 
+/* optimizeaza functiile
+citeste tema inca o data
+coding style
+acolade pe rand cu antetul ca in java
+comentarii
+linii mai mici de 80 de caractere
+whitespace
+readme
+mai multe fisiere + makefile */
 int main()
 {
-	// FILE *pFile;
-	// pFile = fopen("exemplu.in", "r");
+	/* initializeaza directorul radacina */
 	Dir *home = malloc(sizeof(Dir));
 	home->name = "home";
+	home->parent = NULL;
 	home->head_children_files = NULL;
 	home->head_children_dirs = NULL;
-	home->parent = NULL;
-	char *comanda, *nume, *numevechi, *numenou;
+	home->next = NULL;
+	/* citeste prima linie de la tastatura */
 	char *line = malloc(MAX_INPUT_LINE_SIZE * sizeof(char));
 	fgets(line, MAX_INPUT_LINE_SIZE, stdin);
 	do
 	{
-		/* aloca eficient memoria */
-		if (strcmp(line, "\n") != 0)
+		if (strcmp(line, "\n") == 0)
+			continue;
+		/* citeste primul cuvant de pe linie si il copiaza in command */
+		char *token = strtok(line, "\n ");
+		char *command = malloc((1 + strlen(token)) * sizeof(char));
+		strcpy(command, token);
+		/* apeleaza comanda touch */
+		if (strcmp(command, "touch") == 0)
 		{
-			char *token = strtok(line, "\n ");
-			// while (token != NULL)
-			// {
-			// printf(">>%s<<\n", token);
-			comanda = malloc((1 + strlen(token)) * sizeof(char));
-			strcpy(comanda, token);
-			if (strcmp(comanda, "touch") == 0)
-			{
-				token = strtok(NULL, "\n ");
-				/// 1+
-				nume = malloc((1 + strlen(token)) * sizeof(char));
-				strcpy(nume, token);
-				// fscanf(pFile, "%s", nume);
-				// // scanf("%s", nume);
-				touch(home, nume);
-				free(comanda);
-			}
-			else if (strcmp(comanda, "mkdir") == 0)
-			{
-				token = strtok(NULL, "\n ");
-				nume = malloc((1 + strlen(token)) * sizeof(char));
-				strcpy(nume, token);
-				mkdir(home, nume);
-				free(comanda);
-			}
-			else if (strcmp(comanda, "ls") == 0)
-			{
-				ls(home);
-				free(comanda);
-			}
-			else if (strcmp(comanda, "stop") == 0)
-			{
-				Dir *aux = home;
-				while (strcmp(aux->name, "home") != 0)
-					aux = aux->parent;
-				stop(aux);
-				free(comanda);
-			}
-			else if (strcmp(comanda, "rm") == 0)
-			{
-				token = strtok(NULL, "\n ");
-				/* fa si aia cu daca Ana [3] sa fie marimea 4 */
-				nume = malloc((1 + strlen(token)) * sizeof(char));
-				strcpy(nume, token);
-				rm(home, nume);
-				free(comanda);
-				free(nume);
-			}
-			else if (strcmp(comanda, "rmdir") == 0)
-			{
-				token = strtok(NULL, "\n");
-				nume = malloc((1 + strlen(token)) * sizeof(char));
-				strcpy(nume, token);
-				rmdir(home, nume);
-				free(comanda);
-				free(nume);
-			}
-			else if (strcmp(comanda, "cd") == 0)
-			{
-				token = strtok(NULL, "\n");
-				nume = malloc((1 + strlen(token)) * sizeof(char));
-				strcpy(nume, token);
-				cd(&home, nume);
-				free(comanda);
-				free(nume);
-			}
-			else if (strcmp(comanda, "tree") == 0)
-			{
-				tree(home, 0);
-				free(comanda);
-			}
-			else if (strcmp(comanda, "pwd") == 0)
-			{
-				// printf("%s", pwd(home));
-				// pwd(home);
-
-				// pwd(home);
-				char *workingdirectory = pwd(home);
-				printf("%s", workingdirectory);
-				// // if (strcmp(workingdirectory, home->name) != 0)
-				free(workingdirectory);
-				free(comanda);
-			}
-			else if (strcmp(comanda, "mv") == 0)
-			{
-				token = strtok(NULL, "\n ");
-				numevechi = malloc((1 + strlen(token)) * sizeof(char));
-				strcpy(numevechi, token);
-				token = strtok(NULL, "\n ");
-				numenou = malloc((1 + strlen(token)) * sizeof(char));
-				strcpy(numenou, token);
-				mv(home, numevechi, numenou);
-				free(numevechi); // s-ar putea sa trebuiasca stearsa sau mutat in mv exact ca free(newname);
-				free(comanda);
-			}
-			else
-			{
-				free(comanda);
-				continue;
-			}
-			// if (strcmp(comanda, "mv") == 0)
-			// {
-			// 	fscanf(pFile, "%s", numevechi);
-			// 	fscanf(pFile, "%s", numenou);
-			// 	// scanf("%s", numevechi);
-			// 	// scanf("%s", numenou);
-			// 	mv(home, numevechi, numenou);
+			token = strtok(NULL, "\n ");
+			char *name = malloc((1 + strlen(token)) * sizeof(char));
+			strcpy(name, token);
+			touch(home, name);
 		}
+		/* apeleaza comanda mkdir */
+		else if (strcmp(command, "mkdir") == 0)
+		{
+			token = strtok(NULL, "\n ");
+			char *name = malloc((1 + strlen(token)) * sizeof(char));
+			strcpy(name, token);
+			mkdir(home, name);
+		}
+		/* apeleaza comanda ls */
+		else if (strcmp(command, "ls") == 0)
+			ls(home);
+		/* apeleaza comanda rm */
+		else if (strcmp(command, "rm") == 0)
+		{
+			token = strtok(NULL, "\n ");
+			char *name = malloc((1 + strlen(token)) * sizeof(char));
+			strcpy(name, token);
+			rm(home, name);
+		}
+		/* apeleaza comanda rmdir */
+		else if (strcmp(command, "rmdir") == 0)
+		{
+			token = strtok(NULL, "\n");
+			char *name = malloc((1 + strlen(token)) * sizeof(char));
+			strcpy(name, token);
+			rmdir(home, name);
+		}
+		/* apeleaza comanda cd */
+		else if (strcmp(command, "cd") == 0)
+		{
+			token = strtok(NULL, "\n");
+			char *name = malloc((1 + strlen(token)) * sizeof(char));
+			strcpy(name, token);
+			cd(&home, name);
+		}
+		/* apeleaza comanda tree */
+		else if (strcmp(command, "tree") == 0)
+			tree(home, 0);
+		/* apeleaza comanda pwd, afiseara sirul corespunzator */
+		else if (strcmp(command, "pwd") == 0)
+		{
+			char *pathname = pwd(home);
+			printf("%s", pathname);
+			free(pathname);
+		}
+		/* apeleaza comanda mv */
+		else if (strcmp(command, "mv") == 0)
+		{
+			token = strtok(NULL, "\n ");
+			char *oldname = malloc((1 + strlen(token)) * sizeof(char));
+			strcpy(oldname, token);
+			token = strtok(NULL, "\n ");
+			char *newname = malloc((1 + strlen(token)) * sizeof(char));
+			strcpy(newname, token);
+			mv(home, oldname, newname);
+		}
+		/* apeleaza comanda stop, elibereaza memoria */
+		else if (strcmp(command, "stop") == 0)
+		{
+			while (home->parent != NULL)
+				home = home->parent;
+			stop(home);
+		}
+		/* trece peste alte comenzi */
+		else
+		{
+			free(command);
+			continue;
+		}
+		free(command);
 	} while (fgets(line, MAX_INPUT_LINE_SIZE, stdin) != NULL);
-	// while ()
-	// free(home);
+	/* elibereaza memoria */
 	free(line);
-	// fclose(pFile);
 	return 0;
-	/* adauga exceptii */
-	/* maybe fflush stdin? */
 }
